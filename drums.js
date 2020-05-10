d6ca696c2f1dae3b19d1360c018f3eaa7c2edc5f
Arpit Agarwal <57696942+ArpitAgarwal1998@users.noreply.github.com>
2020-05-10 19:43:29 +0530

    Final Upload

--- drums.js
+++ drums.js
@@ -1,10 +1,17 @@
-// buttons = document.querySelectorAll('.btn')
-// for( let value of buttons){
-//   value.addEventListener("click",handleclick);
-// }
+buttons = document.querySelectorAll('.btn');
+for( let value of buttons){
+  value.addEventListener("click",function(){
+    makeSound(this.textContent);
+    displayAnimation(this.textContent);
+  });
+}
 
 document.addEventListener('keypress',function(event){
-  val = event.key;
+  makeSound(event.key);
+  displayAnimation(event.key);
+});
+
+function makeSound(val) {
   switch(val){
     case 'w': audio = new Audio('sounds/tom-1.mp3');
     break;
@@ -17,12 +24,21 @@ document.addEventListener('keypress',function(event){
     case 'j': audio = new Audio('sounds/snare.mp3');
     break;
     case 'k': audio = new Audio('sounds/crash.mp3');
-    break;
-    case 'l': audio = new Audio('sounds/kick-bass.mp3')
+    break ;
+    case 'l': audio = new Audio('sounds/kick-bass.mp3');
     break;
   }
-  audio.play()
-} )
+  audio.play();
+}
 
+function displayAnimation(val){
+  button_pressed = document.querySelector('.'+val);
+  toggleAnimation(button_pressed);
+  setTimeout(toggleAnimation,200,button_pressed);
+}
+
+function toggleAnimation(element){
+  element.classList.toggle('pressed');
+}
   // let audio = new Audio('sounds/tom-1.mp3')
   // audio.play()
